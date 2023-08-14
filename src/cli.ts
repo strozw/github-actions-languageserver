43dc0277a3ec3cd8a6be5b58cfb3adab93363bc5
strozw <strozw@users.noreply.github.com>
2023-08-14 13:08:38 +0900

    update src/cli.ts
    remove `--loader` option

--- src/cli.ts
+++ src/cli.ts
@@ -1,2 +1,2 @@
-#!/usr/bin/env node --experimental-loader @esbuild-kit/esm-loader --loader @esbuild-kit/esm-loader
+#!/usr/bin/env node --experimental-loader @esbuild-kit/esm-loader
 import "@actions/languageserver"
