e6489abec00f19916042bcf71ca71d6ba90560a1
strozw <strozw@users.noreply.github.com>
2023-08-14 14:10:35 +0900

    update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-	entry: ['src/cli.ts'],
-	format: ['esm'],
-	target: 'node18',
+  entry: ['src/cli.ts'],
+  format: ['esm'],
+  target: 'node18',
+  clean: true,
 })
